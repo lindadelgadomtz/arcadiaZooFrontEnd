c0f95e86b54080a2db3630482b4352493f96973c
lindadelgadomtz <linda.dm03@gmail.com>
2024-07-04 19:38:27 +0200

    feat avis: add of page "avis"

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,8 @@ import Route from "./Route.js";
 export const allRoutes = [
 new Route("/", "Accueil", "/pages/home.html"),
 new Route("/services", "Nos Services", "/pages/services.html"),
+new Route("/avis", "Avis", "/pages/avis.html"),
+
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
