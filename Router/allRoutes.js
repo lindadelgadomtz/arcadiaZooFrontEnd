58802abb292f0c563ac0501cc816920aca79dafb
lindadelgadomtz <linda.dm03@gmail.com>
2024-07-21 09:08:22 +0200

    feat: dynamisation du frontend; connexion compte admin et cookies. ajout d'habitats et horaires.html ainsi que des photos pour les habitats

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,8 +5,10 @@ export const allRoutes = [
 new Route("/", "Accueil", "/pages/home.html"),
 new Route("/services", "Nos Services", "/pages/services.html"),
 new Route("/avis", "Avis", "/pages/avis.html"),
-new Route("/connexion", "Connexion", "/pages/connexion.html"),
+new Route("/connexion", "Connexion", "/pages/connexion.html", "/js/auth/connexion.js"),
 new Route("/contact", "Contact", "/pages/contact.html"),
+new Route("/habitats", "Habitats", "/pages/habitats.html"),
+new Route("/horaires", "Nos horaires", "/pages/horaires.html"),
 
 ];
 
