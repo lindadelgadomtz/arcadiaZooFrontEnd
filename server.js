9fd51300e966cee43f4b8538684f064b54b99dad
lindadelgadomtz <linda.dm03@gmail.com>
2024-09-18 01:49:44 +0200

    feat: update server.js

--- server.js
+++ server.js
@@ -4,14 +4,19 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the root directory
+// Serve static files (CSS, JS, etc.)
 app.use(express.static(path.join(__dirname)));
 
-// Serve index.html for all routes
+// Return index.html for any routes that aren't files
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  const routePath = path.join(__dirname, req.path);
+  if (path.extname(routePath)) {
+    res.sendFile(routePath); // Serve the file if it exists
+  } else {
+    res.sendFile(path.join(__dirname, 'index.html')); // Otherwise, serve index.html for SPA
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
